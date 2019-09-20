af75bfcda78ba68ff34dd36db96bfb5dbb13fbf4
kovacs.mercedesz@vereb.dc <kovacs.mercedesz@vereb.dc>
2019-09-20 12:12:16 +0200

    Add project files.

---
+++ Mindegy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindegy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia hogy hívnak?");
+            var nev = Console.ReadLine();
+            Console.WriteLine("Oh milyen szép az, hogy "{nev}"! <3");
+            Console.WriteLine("Szeretsz programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz == "igen")
+            {
+                Console.WriteLine("Akkor mi nagyon jó barátok leszünk");
+            }
+            else
+            {
+                Console.WriteLine(":((( " );
+            }
+            Console.ReadKey();
+        }
+    }
+}
